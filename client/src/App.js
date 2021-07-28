5df71b21e6471f40155147327651efa07cb3bb0a
Pulkit007 <77740386+Pulkit007@users.noreply.github.com>
2021-07-28 16:09:54 +0530

    added redux store

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import { Navbar } from "./components/layout/Navbar";
 import { Landing } from "./components/layout/Landing";
-import { Register } from "./components/auth/Register";
+import Register from "./components/auth/Register";
 import { Login } from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 
 //Redux
 import { Provider } from "react-redux";
@@ -18,8 +19,11 @@ const App = () => {
           <Navbar />
           <Route exact path="/" component={Landing} />
           <section className="container">
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/register" component={Register} />
+            <Alert />
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/register" component={Register} />
+            </Switch>
           </section>
         </Fragment>
       </Router>
